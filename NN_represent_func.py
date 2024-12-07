2a1d5eb1316994826540d8a114a9ac35277f0f9f
Mauro Fava <maurofava.uni@gmail.com>
2024-12-07 12:51:01 +0100

    Add files via upload

---
+++ NN_represent_func.py
@@ -0,0 +1,342 @@
+import sys
+import numpy as np
+from sklearn.model_selection import train_test_split
+from math import sqrt
+
+import torch
+from torch import nn
+import torch.optim as optim
+
+
+from sympy import symbols, sympify
+
+
+import matplotlib
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import streamlit as st
+
+
+sns.set_style('darkgrid')
+matplotlib.rcParams['font.size'] = 14
+matplotlib.rcParams['figure.figsize'] = (10, 6)
+matplotlib.rcParams['figure.facecolor'] = '#00000000'
+
+
+
+    
+
+class inputvals(): # set default input values 
+    def __init__(self, N : int = 100, test_size : float = 0.25, num_layr : int = 2, nneurons : int = 6, P : int = 1000, xmin : float = -2.0, xmax : float = 2.0, wnoise : str = 'y') -> None:
+        self.N = N
+        self.test_size = test_size
+        self.num_layr = num_layr
+        self.nneurons = nneurons
+        self.P = P
+        self.xmin = xmin
+        self.xmax = xmax
+        self.wnoise = wnoise
+    def values(self):
+        return [self.N,self.test_size,self.num_layr,self.nneurons,self.P,self.xmin,self.xmax,self.wnoise]
+    
+           
+
+
+
+
+class NeuralNetwork(nn.Module):
+    def __init__(
+        self,
+        num_layers: int = 1,
+        num_neurons: int = 5,
+    ) -> None:
+        """Basic neural network architecture with linear layers
+        Args:
+            num_layers (int, optional): number of hidden layers
+            num_neurons (int, optional): neurons for each hidden layer
+        """
+        super().__init__()
+        self.num_layers = num_layers
+        self.num_neurons = num_neurons
+        layers = []
+
+        # input layer
+        layers.append(nn.Linear(1, num_neurons))
+
+        # hidden layers
+        for _ in range(num_layers):
+            layers.extend([nn.Linear(num_neurons, num_neurons), nn.Tanh()])
+
+        # output layer
+        layers.append(nn.Linear(num_neurons, 1))
+
+        # build the network
+        self.network = nn.Sequential(*layers)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.network(x.reshape(-1, 1)).squeeze()
+
+
+
+
+
+def trainer(model,function,N,P,testsize,xmin,xmax,wnoise):
+    x = symbols('x')
+    xlist = np.linspace(float(xmin),float(xmax),N)
+    Xtorch = torch.from_numpy(xlist).to(torch.float32).reshape(N,1)
+    ylist = np.array([function.subs(x,xi) for xi in xlist])
+    if wnoise == 'y':
+        print('Add gaussian noise\n')
+        ylist = ylist + np.random.normal(0,0.05,N)
+    if wnoise == 'n':
+        print('No gaussian noise added\n')
+        ylist = ylist
+    ylist = np.array(ylist,dtype=np.float32)
+    Ytorch = torch.from_numpy(ylist)
+    
+    inputs_train, inputs_test, targets_train, targets_test = train_test_split(Xtorch, Ytorch, test_size=testsize)
+
+    criterion = nn.MSELoss()
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+    running_loss = 0.0
+    loss_time = []
+    for epoch in range(0,P):
+        optimizer.zero_grad()
+        outputs = model(inputs_train)
+        loss = criterion(outputs,targets_train)
+        loss.backward()
+        optimizer.step()
+        running_loss += loss.item()
+        loss_time = loss_time + [loss.item()]
+        
+    #print('running loss = ',float(loss))
+    st.write('running loss =', float(loss))
+    loss_time = np.array(loss_time)
+    epochlist = np.linspace(0,P,P)
+    fig, ax = plt.subplots()
+    ax.plot(epochlist,loss_time)
+    ax.set(xlabel='epoch', ylabel='loss', title='loss function')
+    st.pyplot(fig)
+    #plt.plot(epochlist,loss_time,'--m')
+    #plt.xlabel('epoch')
+    #plt.ylabel('loss')
+    #plt.show()
+    return [model,xlist,ylist]
+
+
+
+
+
+def tester(model,N,xarr,yarr):
+    xlist = xarr
+    ylist = yarr
+    xtorch = torch.from_numpy(xlist).to(torch.float32)
+    tw = []
+    for x in xtorch:
+        w = x.reshape(1)
+        tm = model(w).detach().numpy()
+        tw = tw + [tm]
+    
+    tw = np.array(tw)
+    diff = np.array([sqrt((tw[i]-ylist[i])**2) for i in range(0,len(tw))])
+
+    fig, ax = plt.subplots()
+    ax.plot(xlist,ylist,'--r',label='function')
+    ax.plot(xlist,tw,'--b',label='model')
+    ax.plot(xlist,diff,'--g',label='loss')
+    ax.legend()
+    ax.set(xlabel='x', ylabel='y', title='NN-interpolated function')
+    st.pyplot(fig)
+
+
+    
+    #plt.plot(xlist,ylist,'--r',label='function')
+    #plt.plot(xlist,tw,'--b',label='model')
+    #plt.plot(xlist,diff,'--g',label='loss')
+    #plt.axvline(x=xlist[0],color='c')
+    #plt.axvline(x=xlist[len(xlist)-1],color='c')
+    #plt.xlabel('x')
+    #plt.ylabel('y')
+    #plt.legend()
+    #plt.show()
+
+
+
+
+def inputer():
+    sys.stdout.write('Choose Number of points:\n')
+    N = input()
+    sys.stdout.write('Choose the test size:\n')
+    size = input()
+    sys.stdout.write('Choose the number of layers:\n')
+    nlayers = input()
+    sys.stdout.write('Choose the number of neurons:\n')
+    nneurons = input()
+    sys.stdout.write('Choose the number of epochs:\n')    
+    P = input()
+    sys.stdout.write('Choose xmin:\n')
+    xmin = input()
+    sys.stdout.write('Choose xmax:\n')
+    xmax = input()
+    sys.stdout.write('With noise (y or n)?\n')
+    wnoise = input()
+    return [N,size,nlayers,nneurons,P,xmin,xmax,wnoise]
+
+
+
+
+def model_builder(function,N,size,nlayers,nneurons,P,xmin,xmax,wnoise):
+    N, nlayers, nneurons, size, P = int(N), int(nlayers), int(nneurons), float(size), int(P)
+    model = NeuralNetwork(nlayers,nneurons)
+    function = sympify(function) #convert str into function
+    update = trainer(model,function,N,P,size,xmin,xmax,wnoise) # return model, xlist, ylist <- function
+    tester(update[0],N,update[1],update[2])
+    torch.save(model,'model')
+    
+    print(f"Train again?")
+    while True:
+        answer = input("\ny to train again or \nn to quit training\n")
+        if answer.lower() not in ['y','n']:
+            continue
+        if answer.lower() == 'y':
+            get = inputer() # input N, size, epochs, xmin and xmax
+            return model_builder(function,get[0],get[1],get[2],get[3],get[4],get[5],get[6],get[7])
+        if answer.lower() == 'n':
+            print(f'Quit altogether or replot?\n')
+            while True:
+                answer = input('\ny to replot or \nn to exit\n')
+                if answer.lower() == 'y':
+                    oldmod = torch.load('model')
+                    tester(oldmod,N,update[1],update[2])
+                    continue
+                if answer.lower() == 'n':
+                    print("\n🎉🎉🎉🎉")
+                    print("See you the next time!\n")
+                    if __name__ == "__main__":
+                        sys.exit(f"Bye! 👋")
+
+
+
+
+
+    
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        description="Fit 1D function to a Neural Network"
+    )
+
+
+
+    parser.add_argument(
+        '-nX','--ninputs', metavar='Npoints',
+        help='The number of input points.'
+    )
+
+    parser.add_argument(
+        '-s','--size', metavar='testsize',
+        help='Size of the training set.'
+    )
+
+    parser.add_argument(
+        '-p','--pepochs', metavar='Nepochs',
+        help='Number of epochs for the training.'
+    )
+
+    parser.add_argument(
+        '-f','--function', metavar='expression',
+        required=True, help='The function you want to probe'
+    )
+    
+
+    parser.add_argument(
+        '-x0','--xmin', metavar='interval_min',
+        help='The xmin in the interval'
+    )
+
+    parser.add_argument(
+        '-x1','--xmax', metavar='interval_max',
+        help='The xmax in the interval'
+    )
+
+
+
+    parser.add_argument(
+        '-nl','--nlayers', metavar='Nlayers',
+        help='The number of layers'
+    )
+
+    parser.add_argument(
+        '-nn','--nneurons', metavar='Nneurons',
+        help='The number of neurons'
+    )
+
+
+    parser.add_argument(
+        '-wn','--wnoise', metavar='add_noise',
+        help='Logical, w/wo noise'
+    )
+    
+    args = parser.parse_args()
+
+   
+    default = inputvals().values()
+        
+    if args.ninputs == None:
+        ninputs = default[0]
+    else:
+        ninputs = args.ninputs
+        
+    if args.size == None:
+        size = default[1]
+    else:
+        size = args.size
+
+    if args.nlayers == None:
+        nlayers = default[2]
+    else:
+        nlayers = args.nlayers
+
+    if args.nneurons == None:
+        nneurons = default[3]
+    else:
+        nneurons = args.nlayers
+
+    if args.pepochs == None:
+        pepochs = default[4]
+    else:
+        pepochs = args.pepochs
+
+    if args.xmin == None:
+        xmin = default[5]
+    else:
+        xmin = args.xmin
+
+    if args.xmax == None:
+        xmax = default[6]
+    else:
+        xmax = args.xmax
+    if args.wnoise == None:
+        noise = default[7]
+    else:
+        noise = args.wnoise
+    
+
+    print(f"Welcome to the Matrix! 🤖")
+
+    print('N points =',ninputs)
+    print('test size = ',size)
+    print('number of layers = ',nlayers)
+    print('number of neurons = ',nneurons)
+    print('number of epochs = ',pepochs)
+    print('xmin = ',xmin)
+    print('xmax = ',xmax)
+    print('with noise = ',noise)
+
+    
+    model_builder(args.function,ninputs,size,nlayers,nneurons,pepochs,xmin,xmax,noise)
+    
---
+++ simple_function_app.py
@@ -0,0 +1,104 @@
+import streamlit as st
+import time
+import torch
+import matplotlib.pyplot as plt
+import numpy as np
+from math import exp
+import NN_represent_func as mod
+from NN_represent_func import *
+
+
+
+st.title("Build a Neural Network model to interpolate a function")
+
+
+
+
+
+st.write('Interpolate a 1D function of choice over an interval with a Neural Network and with Python\n')
+st.write('\nYou need:')
+st.markdown(
+    """
+    - Python (Numpy, Matplotlib, Seaborn);
+    - Sympy (symbolic expressions);
+    - PyTorch (NN models);
+    - Scikit-Learn (just the train-test splitting).
+    """
+)
+
+
+
+
+
+
+st.sidebar.title("Input parameters")
+function = st.sidebar.text_input("This is the function to be interpolated",'exp(-x**2/2.0)*sin(3.2*x)+cos(1.5*x)')
+#saver = st.sidebar.radio('save trained model?',['y','n'])
+#if saver == 'y':
+namefile = st.sidebar.text_input("Trained model saved under","trained_NN_model")
+wnoise = st.sidebar.radio('w/wo noise', ['y', 'n'])
+xmin = st.sidebar.number_input("xmin",value=-3.0)
+xmax = st.sidebar.number_input("xmax",value=3.0)
+Npoints = st.sidebar.number_input("Number of x points",value=100)
+nlayers = st.sidebar.number_input("Number of layers",value=2)
+nneurons = st.sidebar.number_input("Neurons per layer",value=4)
+size = st.sidebar.number_input("Size of the test set",value=0.25)
+T = st.sidebar.number_input("Number of epochs (training)",value=1000)
+
+
+
+
+
+
+
+
+
+
+
+start = st.button('Train NN model')
+    
+if 'result' not in st.session_state:
+    st.session_state.result = None
+
+if start:
+    try:
+        st.write('The model is being trained')
+        model = mod.NeuralNetwork(nlayers,nneurons)
+        function = sympify(function) #convert str into function
+        update = mod.trainer(model,function,Npoints,T,size,xmin,xmax,wnoise) # return model, xlist, ylist <- function
+        #mod.tester(update[0],Npoints,update[1],update[2])
+        x = 0.1
+        y = x + 3 # final_res generated
+    finally:
+        st.session_state.result = update # update[0] should be the updated model
+        
+
+
+stop = st.button('Save trained NN model')
+
+if stop:
+    something = st.session_state.result[0]
+    Xl = st.session_state.result[1]
+    Yl = st.session_state.result[2]
+    torch.save(something,f'{namefile}')
+    saver = torch.load('trained_NN_model')
+    mod.tester(saver,Npoints,Xl,Yl)
+
+
+
+
+
+count = 1
+file_model = st.file_uploader(label='Upload an existing NN model and (re)plot',key=count)
+if file_model:
+    model = torch.load(file_model)
+    xlist = np.linspace(xmin,xmax,Npoints)
+    x = symbols('x')
+    function = sympify(function)
+    ylist = np.array([function.subs(x,xi) for xi in xlist])#noise not included!
+    #if st.button('replot'):
+    mod.tester(model,Npoints,xlist,ylist)
+    
+
+
+
